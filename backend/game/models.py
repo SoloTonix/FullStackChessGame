ae36e5b26293b2b82504934c9f29aa0f187fd339
Solotonix <solomonokuneye1@gmail.com>
2025-06-05 19:15:36 +0100

    Created Game and Move table

--- backend/game/models.py
+++ backend/game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Game(models.Model):
+    white = 'w'
+    black = 'b'
+    color_choices = [(white, 'White'), (black, 'Black')]
+    
+    white_player = models.ForeignKey(User, on_delete=models.CASCADE, related_name='white_games')
+    plack_player = models.ForeignKey(User, on_delete=models.CASCADE, related_name='black_games')
+    current_fen = models.CharField(max_length=100, default='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')  # Starting position
+    winner = models.CharField(choices=color_choices, max_length=1, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class Move(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='moves')
+    move_text = models.CharField(max_length=10)  # e.g., "e2e4"
+    fen_after = models.CharField(max_length=100)  # Board state after move
+    created_at = models.DateTimeField(auto_now_add=True)
